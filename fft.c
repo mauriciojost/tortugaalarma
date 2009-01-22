6842202e907667655f47cf866d4fa0e6b6dff9e5
mauriciojost <mauriciojost@7a9e7aaa-e62e-11dd-85a8-df16bb8a7048>
2009-01-22 06:10:29 +0000

    Compitiendo contra un algoritmo en Internet. El nuestro (completamente modularizado -> lento) 2.770.000 ciclos. El de Internet (puros bucles -> ultra rapido)              1.670.000 ciclos. Hay mucho por hacer...

--- fft.c
+++ fft.c
@@ -1,9 +1,10 @@
 #include "fft.h"
 #include "complejo.h"#include <stdio.h>
 
+unsigned int n_total;
 
 
-void mariposa(struct complex a, struct complex b, struct complex* A, struct complex* B, int wn)
+void mariposa(struct complex a, struct complex b, struct complex* A, struct complex* B, unsigned int wn)
 {
   struct complex w;
 
@@ -17,15 +18,18 @@ void mariposa(struct complex a, struct complex b, struct complex* A, struct comp
   B->im = a.im-b.im;
   
   // B = (a-b)*W  2° parte...
-  float arg = 3.14159265*2*wn/512;
+  float arg = PI*2*wn/n_total;
 	w.re = cos((double)arg);
 	w.im = sin((double)arg);
   multcmplxp(*B,w,B);  
 }
-void ffttras(struct complex *x,struct complex *y, int n)
-{
-  int i, semi_n=n>>1;
+void ffttras(struct complex *x,struct complex *y, unsigned int n){
+  unsigned  i, semi_n=n>>1;
+  unsigned int len_bits;
+  len_bits = (unsigned int) log2(((float)n));
+  n_total=n;
   
+
   for(i=0;i<semi_n;i++){ 
     mariposa     (x[i],x[i+semi_n] ,&x[i],&x[i+semi_n],i);
   }
@@ -34,18 +38,17 @@ void ffttras(struct complex *x,struct complex *y, int n)
   fft(x+semi_n, semi_n,2);
   
   for(i=0;i<n;i++){ 
-    igcmplxp(x[i],  &y[reversal_bit(i,9)]);
+    igcmplxp(x[i],  &y[reversal_bit(i,len_bits)]);
   }
   
 }
 
 
-void fft(struct complex *x, int len, int profundidad)
+void fft(struct complex *x, unsigned int len, unsigned int profundidad)
 {
   int i, N=len>>1;
   
-  if (len>1)
-  { 
+  if (len>1){ 
     for(i=0;i<N;i++)
       mariposa(x[i],x[i+N],&x[i],&x[i+N],i*profundidad);
     
@@ -70,3 +73,5 @@ unsigned int reversal_bit(unsigned int a, unsigned int n){
   }
   return base;
 }
+
+
