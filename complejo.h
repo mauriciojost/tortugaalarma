b6d1f95fe4d900951f0eabfa8d09c4dbddd4e2a0
mauriciojost <mauriciojost@7a9e7aaa-e62e-11dd-85a8-df16bb8a7048>
2009-01-22 20:57:11 +0000

    Borradas cosas innecesarias. Yendo hacia el paralelismo.

--- complejo.h
+++ complejo.h
@@ -1,6 +1,7 @@
 #ifndef COMPLEJO_H
 #define COMPLEJO_H
 
+#define PI2 3.14159265*2
 #include <math.h>
 
 struct complex
@@ -9,13 +10,12 @@ struct complex
 	double im;
 };
 
-#define PI2 3.14159265*2
+
 
 
 void cargar_vector_complejo(struct complex vector[], int n, float fs, float f);
 void imprimir_vector_complejo(struct complex vector[], int n);
 void imprimir_maximo_modulo(struct complex vector[], int n, float fs);
-void multcmplxp(struct complex *a, struct complex *b, struct complex *c);
 
 #endif
 
