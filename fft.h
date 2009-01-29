dbf8e3fde503b86001bc47df834712c4f0b5eb82
mauriciojost <mauriciojost@7a9e7aaa-e62e-11dd-85a8-df16bb8a7048>
2009-01-29 04:54:41 +0000

    Acomodando algunas cosas menores. Impresiones con otro formato. Intentando hacer el orden de bit reverso paralelo.

--- fft.h
+++ fft.h
@@ -8,8 +8,8 @@ typedef unsigned int uint;
 
 
 inline void mariposa(scomplex *a, scomplex *b, uint wn);
-void fft(scomplex x[], uint len, uint profundidad, uint myrank, uint posicion);
-void ffttras(scomplex *x, scomplex *y, uint n, uint np);
+void dft(scomplex x[], uint len, uint profundidad, uint myrank, uint posicion);
+void fft(scomplex *x, scomplex *y, uint n, uint np);
 uint reversal_bit(uint a, uint n_bits);
 uint logbase2(uint arg);
 uint responsab(uint posic);
