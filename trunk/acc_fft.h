5ffd221513329516e613041f2ecd3ea9dbebb8ec
mauriciojost <mauriciojost@7a9e7aaa-e62e-11dd-85a8-df16bb8a7048>
2009-02-04 11:34:11 +0000

    Arreglada la distribucion del c´odigo en los archivos.

--- trunk/acc_fft.h
+++ trunk/acc_fft.h
@@ -1,4 +1,18 @@
 #ifndef ACC_FFT_H
 #define ACC_FFT_H
-unsigned int corregir_np(unsigned int np);
+
+#include <stdio.h>
+#include "fft.h"
+
+extern uint myrank, np_total, nro_muestras_total;
+extern scomplex *twiddle_factors;
+
+inline void mariposa(scomplex *a, scomplex *b, uint wn);
+uint corregir_np(uint np);
+uint responsab(uint posic);
+uint responsab_ord(uint uP);
+uint reversal_bit(uint a, uint n_bits);
+uint logbase2(uint arg);
+float calcular_suma_verif(uint semi_len, scomplex* senal);
+
 #endif //ACC_FFT_H
--- trunk/fft.h
+++ trunk/fft.h
@@ -4,25 +4,10 @@
 
 #include "complejo.h"
 
-typedef unsigned int uint;
-
-
-inline void mariposa(scomplex *a, scomplex *b, uint wn);
-void dft(scomplex x[], uint len, uint profundidad, uint myrank, uint posicion);
 void fft(scomplex *x, scomplex *y, uint n, uint np);
-uint reversal_bit(uint a, uint n_bits);
-uint logbase2(uint arg);
-uint responsab(uint posic);
-uint reversal_bit(uint a, uint n_bits);
-uint logbase2(uint arg);
-uint responsab(uint posic);
-
-void fft_recibir_tarea();
-uint responsab_ord(uint uP);
+void dft(scomplex x[], uint len, uint profundidad, uint myrank, uint posicion);
 void ordenar_bit_reversal_parcial(scomplex* senal, scomplex* fft_res, uint n, uint np, uint myrank);
-float calcular_suma_verif(uint semi_len, scomplex* senal);
 void derivar_trabajo(scomplex* senal, uint semi_len, uint profundidad, uint respons, uint pos, uint myrank);
-
-
+void fft_recibir_tarea();
 
 #endif /* _FFTM_H */
